e4aa874b37de1314b70785bc916f23a1c8ff60bd
Giacomo Marciani <mgiacomo@amazon.com>
2021-08-11 01:15:59 +0200

    Add custom metric to api/open/hello

--- server/src/main/java/com/yawa/api/open/PublicController.java
+++ server/src/main/java/com/yawa/api/open/PublicController.java
@@ -1,6 +1,8 @@
 package com.yawa.api.open;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class PublicController {
 
+    @Autowired
+    private MeterRegistry meterRegistry;
+
     @GetMapping("/hello")
     public String hello() {
         log.info("Called PublicController.hello");
+        meterRegistry.counter("yawa_custom_counter").increment();
         return "Hello whoever you are, this is a public page";
     }
 }
