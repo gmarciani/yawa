5c5939fb1dfd8ee9cd0b7a0faf233e9b0696dce5
Giacomo Marciani <mgiacomo@amazon.com>
2021-08-14 20:30:40 +0200

    [Server] Add API: /api/open/random

--- server/src/main/java/com/yawa/api/open/PublicController.java
+++ server/src/main/java/com/yawa/api/open/PublicController.java
@@ -1,11 +1,14 @@
 package com.yawa.api.open;
 
+import com.yawa.exceptions.NotAuthorizedException;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping("/api/open")
@@ -20,5 +23,18 @@ public class PublicController {
         log.info("Called PublicController.hello");
         meterRegistry.counter("yawa_custom_counter").increment();
         return "Hello whoever you are, this is a public page";
+    }
+
+    @GetMapping("/random")
+    public String random() {
+        log.info("Called PublicController.random");
+        float dice = new Random().nextFloat();
+        if (dice <= 6.0/10) {
+            return "Success";
+        } else if (dice <= 9.0/10) {
+            throw new NotAuthorizedException("Fake client error");
+        } else {
+            throw new RuntimeException("Fake internal error");
+        }
     }
 }
