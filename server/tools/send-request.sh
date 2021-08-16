0f26217a7472f0e96785a3c524c45cf8d1f0b2bf
Giacomo Marciani <mgiacomo@amazon.com>
2021-08-16 19:49:30 +0200

    [Server] Add tool to send burst of requests to the endpoint /api/open/random

---
+++ server/tools/send-request.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+MAX_REQUESTS=10
+SLEEP_SECONDS=1
+
+function sendRequest() {
+    local method=$1
+    local endpoint=$2
+    local token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkbWluIn0.e3UwvG12weaHaVWZ2u-vuH1SkOb6Ee0NFMVJGtTgwio"
+    curl -k --request $method $endpoint --header "Authorization: Bearer $token"
+}
+
+for request in {1..100}; do
+  echo "Request $request"
+  sendRequest 'GET' 'https://localhost:8002/api/open/random'
+  sleep $SLEEP_SECONDS
+done
