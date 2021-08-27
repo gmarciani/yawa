712e7a7bf4a9940b02a5068d16312edb6980108c
Giacomo Marciani <mgiacomo@amazon.com>
2021-08-28 01:17:38 +0200

    [Server] Refactoring

--- server/src/main/java/com/yawa/api/auth/AuthController.java
+++ server/src/main/java/com/yawa/api/auth/AuthController.java
@@ -3,7 +3,7 @@ package com.yawa.api.auth;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.yawa.exceptions.NotAuthorizedException;
-import com.yawa.models.User;
+import com.yawa.models.users.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
