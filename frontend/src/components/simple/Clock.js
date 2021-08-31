fe673939ed2f746d77d98a83edf1bf62076ab469
Giacomo Marciani <mgiacomo@amazon.com>
2021-08-31 19:19:59 +0200

    [Frontend] Add simple component: Clock

---
+++ frontend/src/components/simple/Clock.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Clock.css';
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        };
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div className="Clock">
+                <p>Time is: {this.state.date.toLocaleTimeString()}.</p>
+            </div>
+        );
+    }
+}
+
+export default Clock;
