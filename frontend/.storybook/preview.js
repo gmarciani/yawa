7e1b8e9a2358dda20b46ad3438a59d4d26f5a30d
Giacomo Marciani <mgiacomo@amazon.com>
2021-09-06 00:04:57 +0200

    [Frontend] Add Bootstrap to the app and to Storybook

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,4 +1,6 @@
 import '../src/index.sass';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.js'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
