ae3a497e6218937933662d098f26ec225c9e846f
Giacomo Marciani <mgiacomo@amazon.com>
2021-09-09 01:18:36 +0200

    [Frontend] First stab at App routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.sass';
+import Login from './components/login/Login';
 import Clock from './components/simple/clock/Clock';
+import Board from './components/board/Board';
 
 function App() {
+  const [token, setToken] = useState();
+
+  /*
+  if(!token) {
+    return <Login setToken={setToken} />
+  }*/
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +30,13 @@ function App() {
           Learn React
         </a>
         <Clock />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/board">
+              <Board />
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
   );
