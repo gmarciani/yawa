af28c08ccc33d1fe0d6e8e2441bbbe0f98e22c35
Giacomo Marciani <mgiacomo@amazon.com>
2021-09-09 23:12:12 +0200

    [Frontend] App: add routing to home, board, profile, login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import React, { useState } from 'react';
 import './App.sass';
 import Login from './components/login/Login';
+import Signup from './components/signup/Signup';
 import Board from './components/board/Board';
+import Profile from './components/profile/Profile';
 import Navbar from "./components/navbar/Navbar";
 
 class App extends React.Component {
@@ -15,6 +17,15 @@ class App extends React.Component {
                       <Switch>
                           <Route path="/board">
                               <Board />
+                          </Route>
+                          <Route path="/profile">
+                              <Profile />
+                          </Route>
+                          <Route path="/login">
+                              <Login />
+                          </Route>
+                          <Route path="/signup">
+                              <Signup />
                           </Route>
                       </Switch>
                   </BrowserRouter>
