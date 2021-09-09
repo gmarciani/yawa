95f59230ffac6e0422714d74d8622035ce7a4c22
Giacomo Marciani <mgiacomo@amazon.com>
2021-09-09 22:39:09 +0200

    [Frontend] Component Login: implemented following the basic.Form structure

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -2,46 +2,177 @@ import React from 'react';
 import './Login.sass';
 import Button from "../simple/button/Button";
 import Alert from "../simple/alert/Alert";
+import FormField from '../simple/form/FormField';
+import { isBlank } from '../../common/string';
+import { jsonString } from '../../common/json';
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
+        this.title = this.props.title || 'Login';
+        this.state = props.state || {
+            data: {
+                email: new FormField(''),
+                password: new FormField(''),
+                rememberMe: new FormField(false)
+            },
+            submitting: false,
+            alert: null
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        event.preventDefault();
+        console.debug(`Handling event: ${event.type} ${event.target.name} (${event.target.type}) = ${event.target.value} ${event.target.checked}`);
+        let targetName = event.target.name;
+        let targetValue = ['checkbox', 'radio'].includes(event.target.type) ? event.target.checked : event.target.value;
+        this.cleanDataValidation(targetName);
+        this.updateDataValue(targetName, targetValue);
+        console.debug(`State is: ${jsonString(this.state)}`);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.debug(`Handling event: ${event.type} ${event.target.name}`);
+        if (this.validateData(this.state.data)) {
+            this.sendData('POST', '/login', this.state.data);
+        } else {
+            this.setState({'alert': {'type': 'warning', 'message': 'Some fields are not valid'}});
+        }
     }
 
     render() {
         let alert;
-        if (this.props.errorMessage) {
+        if (this.state.alert) {
             alert = (
-                <Alert type="error" message={this.props.errorMessage}/>
-            )
-        } else if (this.props.successMessage) {
-            alert = (
-                <Alert type="success" message={this.props.successMessage}/>
+                <Alert type={this.state.alert.type} message={this.state.alert.message}/>
             )
         }
 
         return (
             <div className="LoginWrapper">
-                <h2>Login</h2>
+                <h2>{this.title}</h2>
                 {alert}
-                <form>
+                <form id="login-form" onSubmit={this.handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="input-email" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" disabled={this.props.submitting}/>
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                        <input id="input-email" type="email" name="email" className={"form-control " + this.getFieldValidationClassNames('email')} placeholder="me@myself.com" value={this.state.data.email.value} disabled={this.state.submitting} onChange={this.handleChange}/>
+                        <div className="invalid-feedback">
+                            {this.state.data.email.errorMessage}
+                        </div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="input-password" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password" disabled={this.props.submitting}/>
+                        <input id="input-password" type="password" name="password" className={"form-control " + this.getFieldValidationClassNames('password')} placeholder="password" value={this.state.data.password.value} disabled={this.state.submitting} onChange={this.handleChange}/>
+                        <div className="invalid-feedback">
+                            {this.state.data.password.errorMessage}
+                        </div>
                     </div>
-                    <div className="mb-3 form-check">
-                        <input type="checkbox" className="form-check-input" id="remember-me" disabled={this.props.submitting}/>
-                        <label htmlFor="input-remember-me" className="form-check-label">Rememeber Me</label>
+                    <div className="mb-3 form-check form-switch">
+                        <input id="input-remember-me" type="checkbox" name="checkMe" className={"form-check-input " + this.getFieldValidationClassNames('rememberMe')} checked={this.state.data.rememberMe.value} disabled={this.state.submitting} onChange={this.handleChange}/>
+                        <label htmlFor="input-remember-me" className="form-check-label">Remember Me</label>
+                        <div className="invalid-feedback">
+                            {this.state.data.rememberMe.errorMessage}
+                        </div>
                     </div>
-                    <Button value={this.props.submitting && "Submitting" || "Submit"} type="submit" loading={this.props.submitting} disabled={this.props.submitDisabled}/>
+                    <Button value={this.state.submitting && "Submitting" || "Submit"} type="submit" loading={this.state.submitting}/>
                 </form>
             </div>
         );
+    }
+
+    updateDataValue(name, value) {
+        const { data } = { ...this.state };
+        data[name].value = value;
+        this.setState({'data': data});
+    }
+
+    validateData(data) {
+        console.log(`Validating data: ${jsonString(data)}`);
+        let errors = 0;
+        this.validateEmail(data.email, data) || errors++;
+        this.validatePassword(data.password, data) || errors++;
+        this.validateRememberMe(data.rememberMe, data) || errors++;
+        console.log(`State after validation: ${jsonString(this.state)}`);
+        return errors === 0;
+    }
+
+    sendData(method, url, data) {
+        console.log(`Sending request to ${method} ${url} with body: ${jsonString(data)}`);
+        this.setState({'submitting': true});
+        return fetch(url, {
+            method: method,
+            body: jsonString(data),
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then(response => response.json())
+            .then(response => {
+                console.log(`Response from ${method} ${url}: ${jsonString(response)}`);
+                let alertType = this.getAlertTypeFromResponseStatus(response.status);
+                let alertMessage = response.message;
+                this.setState({'alert': {'type': alertType, 'message': alertMessage}});
+            }).catch(error => {
+                console.log(`Error from ${method} ${url}: ${error.message}`);
+                let alertType = 'error';
+                let alertMessage = error.message;
+                this.setState({'alert': {'type': alertType, 'message': alertMessage}});
+            }).finally(() => {
+                this.setState({'submitting': false});
+            });
+    }
+
+    validateEmail(field, data) {
+        if (isBlank(field.value)) {
+            console.log(`email is invalid: ${field.value}`);
+            field.setInvalid(`email must not be empty`);
+            return false;
+        }
+        field.setValid();
+        return true;
+    }
+
+    validatePassword(field, data) {
+        if (isBlank(field.value)) {
+            console.log(`password is invalid: ${field.value}`);
+            field.setInvalid(`password must not be empty`);
+            return false;
+        }
+        field.setValid();
+        return true;
+    }
+
+    validateRememberMe(field, data) {
+        field.setValid();
+        return true;
+    }
+
+    cleanDataValidation(field) {
+        const { data } = { ...this.state };
+        data[field].cleanValidation();
+        this.setState({'data': data});
+    }
+
+    getFieldValidationClassNames(field) {
+        switch(this.state.data[field].isValid) {
+            case true: return 'is-valid';
+            case false: return 'is-invalid';
+            default: return '';
+        }
+    }
+
+    getAlertTypeFromResponseStatus(status) {
+        if (status >= 200 && status < 300) {
+            return 'success';
+        } else if (status >= 400 && status < 500) {
+            return 'warning';
+        } else if (status >= 500 && status < 600) {
+            return 'error';
+        } else {
+            return 'info';
+        }
     }
 }
 
