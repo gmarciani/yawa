8aa499884abb413dd7ebb0610a4beb8d7505a250
Giacomo Marciani <mgiacomo@amazon.com>
2021-09-09 22:49:16 +0200

    [Frontend] App: remove unused code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.sass';
 import Login from './components/login/Login';
-import Clock from './components/simple/clock/Clock';
 import Board from './components/board/Board';
+import Navbar from "./components/navbar/Navbar";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  render() {
-      return (
-          <div className="App">
-              <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <p>Edit <code>src/App.js</code> and save to reload</p>
-                  <a className="App-link"  href="https://reactjs.org"  target="_blank"  rel="noopener noreferrer">Learn React</a>
-                  <Clock />
+    render() {
+          return (
+              <div className="App">
+                  <Navbar/>
                   <BrowserRouter>
                       <Switch>
                           <Route path="/board">
@@ -26,10 +18,9 @@ class App extends React.Component {
                           </Route>
                       </Switch>
                   </BrowserRouter>
-              </header>
-          </div>
-      );
-  }
+              </div>
+          );
+    }
 }
 
 export default App;
