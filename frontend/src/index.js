a14ddee2cfad2ceca7e5761a0088e369ec0a473f
Giacomo Marciani <mgiacomo@amazon.com>
2021-09-10 22:15:49 +0200

    [Frontend] Start mock backend without promises because it creates issues to live reloading

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,22 +4,17 @@ import './index.sass';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function prepare() {
-    if (process.env.NODE_ENV === 'development') {
-        const { worker } = require('./mocks/browser')
-        return worker.start()
-    }
-    return Promise.resolve()
+if (process.env.NODE_ENV === 'development') {
+    const { worker } = require('./mocks/browser');
+    worker.start();
 }
 
-prepare().then(() => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-});
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
