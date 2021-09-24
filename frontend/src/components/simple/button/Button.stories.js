c2d44e23a0c001fb2ba1fb10b51cc294027484c7
Giacomo Marciani <mgiacomo@amazon.com>
2021-09-24 14:04:20 +0200

    [Frontend] Component simple.Button: add Storybook docs and revised stories

--- frontend/src/components/simple/button/Button.stories.js
+++ frontend/src/components/simple/button/Button.stories.js
@@ -4,13 +4,46 @@ import Button from "./Button";
 export default {
     component: Button,
     title: 'Button',
+    args: {
+        type: 'button',
+        value: 'Label',
+        disabled: false,
+        loading: false
+    },
+    argTypes: {
+        type: {
+            description: 'Set the type of the &lt;button&gt; element. A submit button is meant to be included in a &lt;form&gt;.',
+            control: { type: 'radio' },
+            options: ['button', 'submit']
+        },
+        value: {
+            description: 'The button label.',
+            default: 'Button'
+        },
+        disabled: {
+            description: 'Toggles the enabled/disabled state of the button',
+            default: false
+        },
+        loading: {
+            description: 'Toggles the loading state of the button. A loading button is disabled by default.',
+            default: false
+        }
+    }
 };
 
 const Template = args => <Button {...args}/>;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Basic = Template.bind({});
+Basic.args = {
     value: "Click Me"
+};
+
+export const Custom = Template.bind({});
+Custom.args = {
+    value: "Click Me",
+    disabled: false,
+    loading: false,
+    type: "button"
 };
 
 export const Disabled = Template.bind({});
@@ -27,13 +60,13 @@ Loading.args = {
 
 export const Submit = Template.bind({});
 Submit.args = {
+    type: "submit",
     value: "Submit",
-    type: "submit"
 };
 
 export const Submitting = Template.bind({});
 Submitting.args = {
-    value: "Submitting",
     type: "submit",
+    value: "Submitting",
     loading: true
 };
