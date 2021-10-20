f3ac6206b2d7694f5354e4013406f98d02c6bf47
Giacomo Marciani <mgiacomo@amazon.com>
2021-10-20 22:45:52 +0200

    [Server] Add dependencies for testing

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -44,7 +44,9 @@ dependencies {
 
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 
 	//annotationProcessor("org.projectlombok:lombok:1.18.20")
 	//compileOnly("org.projectlombok:lombok:1.18.20")
@@ -54,8 +56,10 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("mysql:mysql-connector-java:8.0.26")
 
+	testImplementation("io.kotest:kotest-framework-api-jvm:4.6.0")
+	testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
+	testImplementation("io.mockk:mockk:1.11.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("junit:junit:4.13.2")
 }
 
 tasks.withType<KotlinCompile> {
