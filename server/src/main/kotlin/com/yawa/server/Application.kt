db23e158ac6dd65c349cfccb06f7f32c6c3844de
Giacomo Marciani <mgiacomo@amazon.com>
2023-04-24 10:53:15 +0200

    [Server] Remove print of beans definition at startup.

--- server/src/main/kotlin/com/yawa/server/Application.kt
+++ server/src/main/kotlin/com/yawa/server/Application.kt
@@ -1,11 +1,8 @@
 package com.yawa.server
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.ApplicationPidFileWriter
 import org.springframework.boot.runApplication
-import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class Application
@@ -13,13 +10,5 @@ class Application
 fun main(args: Array<String>) {
     runApplication<Application>(*args) {
         addListeners(ApplicationPidFileWriter())
-    }
-}
-
-@Bean
-fun commandLineRunner(ctx: ApplicationContext) : CommandLineRunner {
-    return CommandLineRunner {
-        println("Beans:")
-        ctx.beanDefinitionNames.sorted().forEach { println(it) }
     }
 }
