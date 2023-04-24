58744ef93c8860d4fb0180c5e4055a44a1cd8ea2
Giacomo Marciani <mgiacomo@amazon.com>
2023-04-24 10:52:42 +0200

    [Server] Set default content-type: application/json.

--- server/src/main/kotlin/com/yawa/server/components/mvc/AppMvcConfig.kt
+++ server/src/main/kotlin/com/yawa/server/components/mvc/AppMvcConfig.kt
@@ -4,7 +4,9 @@ import com.yawa.server.components.mvc.interceptors.AccessLogger
 import com.yawa.server.components.mvc.interceptors.RequestIdAssigner
 import com.yawa.server.components.mvc.interceptors.RequestOperationNameAssigner
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -14,6 +16,11 @@ class AppMvcConfig(
     @Autowired val requestOperationNameAssigner: RequestOperationNameAssigner,
     @Autowired val accessLogger: AccessLogger
 ) : WebMvcConfigurer {
+
+    override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
+        super.configureContentNegotiation(configurer)
+        configurer.defaultContentType(MediaType.APPLICATION_JSON)
+    }
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(requestIdAssigner)
