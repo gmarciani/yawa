a51bc2cb6c7d066cba5231e2ce403795a6194850
Giacomo Marciani <mgiacomo@amazon.com>
2023-05-22 09:28:27 +0200

    [Server] Remove user from database on user deletion confirmed event.

--- server/src/main/kotlin/com/yawa/server/events/users/deletion/UserDeletionConfirmedListener.kt
+++ server/src/main/kotlin/com/yawa/server/events/users/deletion/UserDeletionConfirmedListener.kt
@@ -3,6 +3,7 @@ package com.yawa.server.events.users.deletion
 import com.yawa.server.models.users.User
 import com.yawa.server.notifications.MailService
 import com.yawa.server.notifications.MailType
+import com.yawa.server.services.UserService
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationListener
@@ -12,11 +13,13 @@ private val log = KotlinLogging.logger {}
 
 @Component
 class UserDeletionConfirmedListener(
+    @Autowired val userService: UserService,
     @Autowired val mailService: MailService
 ): ApplicationListener<UserDeletionConfirmedEvent> {
     override fun onApplicationEvent(event: UserDeletionConfirmedEvent) {
         val user = event.user
         log.info("ON EVENT ${event.javaClass.simpleName}: handling event for user ${user.username}")
+        userService.deleteUser(user)
         sendEmail(user)
     }
 
