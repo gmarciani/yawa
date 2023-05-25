55c4d067cfc91396fe0ebf9e24ea8c41f1158061
Giacomo Marciani <mgiacomo@amazon.com>
2023-05-25 23:14:27 +0200

    [Server] Relax throttling limits.

--- server/src/main/kotlin/com/yawa/server/models/users/UserSubscriptionPlan.kt
+++ server/src/main/kotlin/com/yawa/server/models/users/UserSubscriptionPlan.kt
@@ -6,13 +6,13 @@ import java.time.Duration
 
 enum class UserSubscriptionPlan {
     FREE {
-        override fun limit(): Bandwidth = Bandwidth.classic(20, Refill.intervally(20, Duration.ofHours(1)))
+        override fun limit(): Bandwidth = Bandwidth.classic(20, Refill.intervally(20, Duration.ofMinutes(1)))
     },
     BASIC {
-        override fun limit(): Bandwidth = Bandwidth.classic(40, Refill.intervally(40, Duration.ofHours(1)))
+        override fun limit(): Bandwidth = Bandwidth.classic(40, Refill.intervally(40, Duration.ofMinutes(1)))
     },
     PROFESSIONAL {
-        override fun limit(): Bandwidth = Bandwidth.classic(100, Refill.intervally(100, Duration.ofHours(1)))
+        override fun limit(): Bandwidth = Bandwidth.classic(100, Refill.intervally(100, Duration.ofMinutes(1)))
     },
     SYSTEM {
         override fun limit(): Bandwidth = Bandwidth.classic(1000, Refill.intervally(1000, Duration.ofMinutes(1)))
