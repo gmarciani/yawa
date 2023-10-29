1ce0c2012dacc1a04e4a239b74a6b63063981708
Giacomo Marciani <mgiacomo@amazon.com>
2023-10-29 17:33:05 +0100

    [Frontend] Remove purchase button and related envars.

--- frontend/src/_metronic/partials/layout/RightToolbar.tsx
+++ frontend/src/_metronic/partials/layout/RightToolbar.tsx
@@ -8,7 +8,7 @@ const RightToolbar: FC = () => {
     <>
       <div className='engage-toolbar d-flex position-fixed px-5 fw-bolder zindex-2 top-50 end-0 transform-90 mt-20 gap-2'>
         <ToggleHelpDrawer />
-        <PurchaseButton />
+        {/*<PurchaseButton />*/}
       </div>
 
       <HelpDrawer />
