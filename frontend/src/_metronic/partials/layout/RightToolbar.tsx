1c4be5c83af6f84b6d961a1f59907e5b350e4b38
Giacomo Marciani <mgiacomo@amazon.com>
2023-10-29 20:50:53 +0100

    [Frontend] Conditional rendering of purchase button.

--- frontend/src/_metronic/partials/layout/RightToolbar.tsx
+++ frontend/src/_metronic/partials/layout/RightToolbar.tsx
@@ -4,11 +4,15 @@ import {HelpDrawer} from './help-drawer/HelpDrawer'
 import {PurchaseButton} from './purchase/PurchaseButton'
 
 const RightToolbar: FC = () => {
+  // const showPurchaseButton = String(process.env.REACT_APP_PURCHASE_SHOW_BUTTON).toLowerCase() === 'true'
+  const showPurchaseButton = process.env.REACT_APP_PURCHASE_SHOW_BUTTON === 'true'
+  // const showPurchaseButton = false
+
   return (
     <>
       <div className='engage-toolbar d-flex position-fixed px-5 fw-bolder zindex-2 top-50 end-0 transform-90 mt-20 gap-2'>
         <ToggleHelpDrawer />
-        {/*<PurchaseButton />*/}
+        { showPurchaseButton && <PurchaseButton /> }
       </div>
 
       <HelpDrawer />
