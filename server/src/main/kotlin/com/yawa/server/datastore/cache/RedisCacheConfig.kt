54754c207a509e45b482c5b11e6b4b56d87a1e21
Giacomo Marciani <mgiacomo@amazon.com>
2023-11-21 16:43:09 +0100

    [Server] Add yaml configurations related to Cache + minor changes to the DB configurations.

--- server/src/main/kotlin/com/yawa/server/datastore/cache/RedisCacheConfig.kt
+++ server/src/main/kotlin/com/yawa/server/datastore/cache/RedisCacheConfig.kt
@@ -1,23 +1,22 @@
 package com.yawa.server.cache
 
 import io.lettuce.core.RedisClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 
 @Configuration
-class RedisCacheConfig {
-
-    companion object {
-        const val REDIS_USER = "admin"
-        const val REDIS_PASSWORD = "adminpass"
-        const val REDIS_HOST = "cache"
-        const val REDIS_PORT = 6379
-    }
+class RedisCacheConfig(
+    @Value("\${yawa.cache.username}") final val username: String,
+    @Value("\${yawa.cache.password}") final val password: String,
+    @Value("\${yawa.cache.host}") final val host: String,
+    @Value("\${yawa.cache.port}") final val port: Int,
+) {
 
     @Bean
     fun redisClient(): RedisClient {
-        return RedisClient.create("redis://$REDIS_USER:$REDIS_PASSWORD@$REDIS_HOST:$REDIS_PORT");
+        return RedisClient.create("redis://$username:$password@$host:$port")
     }
 
 }
