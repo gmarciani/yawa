3094ca7c5399160f9cfc4875bead2c8d39a3f9fb
Giacomo Marciani <mgiacomo@amazon.com>
2025-03-30 01:40:28 -0400

    [Fronted] Remove deprecated pages from auth routes.

--- frontend/src/app/modules/auth/AuthPage.tsx
+++ frontend/src/app/modules/auth/AuthPage.tsx
@@ -3,8 +3,6 @@ import {Registration} from './components/Registration'
 import {ForgotPassword} from './components/ForgotPassword'
 import {Login} from './components/Login'
 import {AuthLayout} from './AuthLayout'
-import {ActivateUser} from "./components/ActivateUser";
-import {ResetPassword} from "./components/ResetPassword";
 
 const AuthPage = () => (
   <Routes>
